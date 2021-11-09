412c5ed215585179a1b4fb26f4f85639d62f2c06
neardybhaskar <neardybhaskar@gmail.com>
2021-11-10 00:22:41 +0530

    Code changes

--- HelloTestNew.java
+++ HelloTestNew.java
@@ -1,11 +1,19 @@
+import java.io.IOException;
 /**
  * @author Bhaskar Singh
  * @date 11/9/2021 4:42 PM
  */
 public class HelloTestNew {
+	
+	private static String testVariable = "TEST";
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+		try {
+            Runtime.getRuntime().exec("export HelloTest = " +testVariable);
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
     }
 
 }
