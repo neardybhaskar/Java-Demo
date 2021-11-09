e400a694cd4bb9a5d6d4337515af6a047b279455
neardybhaskar <neardybhaskar@gmail.com>
2021-11-09 18:07:35 +0530

    first commit

---
+++ HelloTestNew.java
@@ -0,0 +1,11 @@
+/**
+ * @author Bhaskar Singh
+ * @date 11/9/2021 4:42 PM
+ */
+public class HelloTestNew {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
